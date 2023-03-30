501c1e2178d86cba407414c861f199d5ffa21575
karsyn ansari <karsyn.ansari@gmail.com>
2023-03-29 22:18:39 -0600

    1 & 2 solved

---
+++ 2/index-2.js
@@ -0,0 +1,91 @@
+const fs = require("fs");
+
+const data = fs.readFileSync("input.txt").toString();
+const newData = data.split(/\r?\n|\r|\n/g);
+
+//loose combo
+const lose = {
+  1: 3,
+  2: 1,
+  3: 2,
+};
+
+//win combo
+const win = {
+  1: 2,
+  2: 3,
+  3: 1,
+};
+
+
+let newerData = [];
+for (const element of newData) {
+  let obj = {};
+  const opponent = element.split(" ")[0];
+
+  if (opponent === "A") {
+    obj["opponent"] = 1;
+  }
+  if (opponent === "B") {
+    obj["opponent"] = 2;
+  }
+
+  if (opponent === "C") {
+    obj["opponent"] = 3;
+  }
+  obj["you"] = element.split(" ")[1];
+  newerData.push(obj);
+}
+
+// console.log(newerData);
+
+const finalPlay = () => {
+  for (const element of newerData) {
+    if (element.you === "X") {
+      element.you = lose[element.opponent];
+    }
+
+    if (element.you === "Y") {
+      element.you = element.opponent;
+    }
+
+    if (element.you === "Z") {
+      element.you = win[element.opponent];
+    }
+  }
+  return newerData;
+};
+const finalPlayArray = finalPlay();
+
+const getFinalScore = (arraytocalculate) => {
+  let sumArray = [];
+  let sum = 0;
+  for (const element of arraytocalculate) {
+    if (element.opponent === element.you) {
+      sum += 3 + element.you;
+      sumArray.push(sum);
+    }
+    if (
+      (element.opponent === 1 && element.you === 3) ||
+      (element.opponent === 3 && element.you === 2) ||
+      (element.opponent === 2 && element.you === 1)
+    ) {
+      sum += element.you;
+      sumArray.push(sum);
+    }
+
+    if (
+      (element.opponent === 1 && element.you === 2) ||
+      (element.opponent === 2 && element.you === 3) ||
+      (element.opponent === 3 && element.you === 1)
+    ) {
+      sum += 6 + element.you;
+      sumArray.push(sum);
+    }
+    sum = 0;
+  }
+  const thesum = sumArray.reduce((total, item) => total + item);
+
+  return thesum;
+};
+console.log(getFinalScore(finalPlayArray));
